efc5fc52a87909c8f21ed80e267c64ae7f56ee8d
sathiiii <sathirasofte@gmail.com>
2022-09-29 11:32:53 +0530

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,30 @@ var scroll = 0;
 var active = false;
 
 $(function() {
+    $('.chip').click(function() {
+        $('.chip-active').map(function() {
+            $(this).removeClass('chip-active');
+        });
+        $(this).addClass('chip-active');
+        var tag = $(this).attr('data-tag');
+        $('.chip-content').map(function() {
+            var technologies = $(this).attr('data-technologies');
+            if (technologies) {
+                technologies = technologies.split(', ');
+                technologies = technologies.map(function(value) {
+                    return value.replace(/\s/g, '');
+                });
+            }
+            if (tag == 'all' || $(this).attr('data-scope') == tag || (technologies && technologies.indexOf(tag) > -1)) {
+                $(this).css('display', 'block');
+                $(this).css('opacity', '0');
+                $(this).animate({
+                    opacity: 1
+                }, 400);
+            }
+            else $(this).css('display', 'none');
+        });
+    });
     $(window).bind('mousewheel', function(event, delta) {
         var body = document.body, html = document.documentElement;
         var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
